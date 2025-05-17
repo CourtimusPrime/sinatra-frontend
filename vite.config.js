2bd7da1675853edecf293e8e1e199eba511e3338
CourtimusPrime <dashdale02@gmail.com>
2025-05-17 13:06:28 +0400

    chore: add alias for framer-motion in vite config

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,26 @@
 // vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      'framer-motion': path.resolve(__dirname, 'node_modules/framer-motion/dist/framer-motion.es.js'),
+    },
+  },
   publicDir: 'public',
   build: {
     outDir: 'dist',
-    chunkSizeWarningLimit: 1000,
     rollupOptions: {
+      input: 'index.html',
       output: {
         manualChunks: {
           vendor: ['react', 'react-dom', 'react-router-dom'],
         },
       },
     },
+    chunkSizeWarningLimit: 1000,
   },
 });
